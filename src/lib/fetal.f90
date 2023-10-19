8d381b9901c51c5232a476bc69764a670598dc4b
Alys Clark <acla148@bn405952.uoa.auckland.ac.nz>
2023-10-19 15:29:20 +1300

    reading in groups

--- src/lib/fetal.f90
+++ src/lib/fetal.f90
@@ -78,6 +78,86 @@ contains
         node_field_fetal(njf_comp,:) = node_xyz_fetal(3,:)
         filename = 'group.exelem'
         call import_exelemfield(filename,ne_group)
+        !! HARD CODED - TO BE READ IN AS FIELDS
+        !Compartment 1 - Right ventricle
+         node_field_fetal(njf_type,1) = 1.0_dp !The right ventricle
+        node_field_fetal(njf_press,1) = 869.82_dp!4.0_dp*133.0_dp
+        node_field_fetal(njf_comp,1) = 0.0_dp!1.0_dp/EdiaRV
+
+        !!Compartment 2 - Left ventricle
+         node_field_fetal(njf_type,2) = 2.0_dp !The left ventricle
+         node_field_fetal(njf_press,2) = 873.81_dp! 3.0*133.0_dp
+         node_field_fetal(njf_comp,2) = 0.0_dp
+         !!Compartment 3 - Right atrium
+        node_field_fetal(njf_type,3) = 3.0_dp !Any atrium
+         node_field_fetal(njf_press,3) = 279.30_dp!2.1_dp*133.0_dp
+         node_field_fetal(njf_comp,3) = 2.0_dp*1000.0_dp/133.0_dp !ml/mmHg to mm3/Pa.
+         !!Compartment 4 - Left atrium
+        node_field_fetal(njf_type,4) = 3.0_dp !Any atrium
+         node_field_fetal(njf_press,4) = 399.0_dp! 3.0_dp*133.0_dp !m
+         node_field_fetal(njf_comp,4) = 1.0_dp*1000.0_dp/133.0_dp !ml/mmHg to mm3/Pa.
+         !!Compartment 5 PA1
+         node_field_fetal(njf_press,5) = 5852.0_dp! 44.0_dp*133.0_dp !m
+         node_field_fetal(njf_comp,5) = 0.08*1000.0_dp/133.0_dp !ml/mmHg to mm3/Pa
+         do np = 5,num_nodes_fetal
+            node_field_fetal(njf_type,np) = 4.0_dp !Generic compartment
+        end do
+        !!!AA
+        node_field_fetal(njf_press,6) = 5905.20_dp!44.4_dp*133.0_dp !Pa
+        node_field_fetal(njf_comp,6) = 0.37593984962406_dp !mm3/Pa
+        !!AO1
+        node_field_fetal(njf_press,7) = 5732.30 !Pa
+        node_field_fetal(njf_comp,7) = 0.601503759398496_dp
+        !!AO2
+        node_field_fetal(njf_press,8) = 5506.2_dp!Pa
+        node_field_fetal(njf_comp,8) = 0.526315789473684_dp
+        !!Ao3
+        node_field_fetal(njf_press,9) = 5426.4_dp!Pa
+        node_field_fetal(njf_comp,9) = 0.300751879699248_dp
+        !!AO4
+        node_field_fetal(njf_press,10) = 5359.9_dp!Pa
+        node_field_fetal(njf_comp,10) = 0.37593984962406_dp
+         !!!PA2
+         node_field_fetal(njf_press,11) = 5732.3_dp!Pa
+         node_field_fetal(njf_comp,11) = 0.601503759398496_dp
+        !!Lung
+        node_field_fetal(njf_press,12) = 1463.0_dp!Pa
+        node_field_fetal(njf_comp,12) = 3.00751879699248_dp
+        !CA
+        node_field_fetal(njf_press,13) = 5599.3_dp!Pa
+        node_field_fetal(njf_comp,13) = 0.075187969924812_dp
+         !!BR
+         node_field_fetal(njf_press,14) = 4309.2_dp!Pa
+         node_field_fetal(njf_comp,14) = 2.25563909774436_dp
+        !!SVC
+        node_field_fetal(njf_press,15) = 625.1_dp!Pa
+        node_field_fetal(njf_comp,15) = 7.5187969924812_dp
+        !!UB
+        node_field_fetal(njf_press,16) = 2566.9_dp!Pa
+         node_field_fetal(njf_comp,16) = 6.39097744360902_dp
+        !!HE
+         node_field_fetal(njf_press,17) = 678.3_dp!Pa
+        node_field_fetal(njf_comp,17) = 22.5563909774436_dp
+        !!INTE
+        node_field_fetal(njf_press,18) = 1476.3_dp!Pa
+         node_field_fetal(njf_comp,18) = 1.8796992481203_dp
+         !!KID
+         node_field_fetal(njf_press,19) = 4269.3_dp!Pa
+         node_field_fetal(njf_comp,19) =  0.150375939849624_dp
+         !!IVC
+         node_field_fetal(njf_press,20) = 571.9!Pa
+         node_field_fetal(njf_comp,20) = 4.51127819548872_dp
+        !!PLAC
+        node_field_fetal(njf_press,21) = 2979.2_dp!Pa
+        node_field_fetal(njf_comp,21) = 11.2781954887218_dp !mm3/Pa
+         !!UV
+         node_field_fetal(njf_press,22) = 891.1_dp!Pa
+        node_field_fetal(njf_comp,22) = 2.25563909774436_dp
+        !!LE
+        node_field_fetal(njf_press,23) = 1356.6_dp!Pa
+        node_field_fetal(njf_comp,23) = 30.0751879699248_dp !mm3/Pa!
+
+
 
         do np = 1,num_nodes_fetal
             if(np.le.2)then
@@ -92,70 +172,70 @@ contains
 
 
         !ELement 1, 1-5 RV-PA1, one way flow should occur when RV pressure > PA pressure
-        !elem_field_fetal(ne_group,1) = 8.0_dp !One way valve
+        elem_field_fetal(ne_group,1) = 8.0_dp !One way valve
         elem_field_fetal(ne_resist,1) = 0.0_dp
         elem_field_fetal(nef_K,1) = 0.001_dp *133.0_dp/(1000.0_dp*1000.0_dp)!mmHg s2/ml2 -> Pa.s2/mm6
         elem_field_fetal(nef_L,1) = 0.0_dp
         !Elt2 2-6 LV-AA, one way flow, should occur when LV pressure > AA pressure
-        !elem_field_fetal(ne_group,2) = 8.0_dp !One way valve
+        elem_field_fetal(ne_group,2) = 8.0_dp !One way valve
         elem_field_fetal(ne_resist,2) = 0.0_dp
         elem_field_fetal(nef_K,2) = 0.001_dp *133.0_dp/(1000.0_dp*1000.0_dp)!mmHg s2/ml2 -> Pa.s2/mm6
         elem_field_fetal(nef_L,2) = 0.0_dp
         !Elt 3 3-15 RA-SVC, standard 2 2way flow
-        !elem_field_fetal(ne_group,3) = 2.0_dp !Simple R-Q unit
+        elem_field_fetal(ne_group,3) = 2.0_dp !Simple R-Q unit
         elem_field_fetal(ne_resist,3) = 0.0266644736_dp ! Pa s /mm3
         elem_field_fetal(nef_K,3) = 0.0_dp
         elem_field_fetal(nef_L,3) = 0.0_dp
         !Elt 4 3-1 RA-RV !One way flow from atrium to ventricle
-        !elem_field_fetal(ne_group,4) = 1.0_dp !One way valve
+        elem_field_fetal(ne_group,4) = 1.0_dp !One way valve
         elem_field_fetal(ne_resist,4) = 0.0_dp
         elem_field_fetal(nef_K,4) = 0.002_dp *133.0_dp/(1000.0_dp*1000.0_dp)!mmHg s2/ml2 -> Pa.s2/mm6
         elem_field_fetal(nef_L,4) = 0.0016_dp*133.0_dp/1000.0_dp !mmHg s2/ml - Pa . s2/mm3
 
         !Elt 5 4-12 LA to lung, standard 2 way flow
-        !elem_field_fetal(ne_group,5) = 2.0_dp !Simple R-Q unit
+        elem_field_fetal(ne_group,5) = 2.0_dp !Simple R-Q unit
         elem_field_fetal(ne_resist,5) = 0.266644736_dp ! Pa s /mm3
         elem_field_fetal(nef_K,5) = 0.0_dp
         elem_field_fetal(nef_L,5) = 0.0_dp
 
         !Elt 6 4-2 LA-LV One way flow from atrium to ventricle
-        !elem_field_fetal(ne_group,6) = 1.0_dp !One way valve
+        elem_field_fetal(ne_group,6) = 1.0_dp !One way valve
         elem_field_fetal(ne_resist,6) = 0.0_dp
         elem_field_fetal(nef_K,6) = 0.002_dp *133.0_dp/(1000.0_dp*1000.0_dp)!mmHg s2/ml2 -> Pa.s2/mm6
         elem_field_fetal(nef_L,6) = 0.0016_dp*133.0_dp/1000.0_dp !mmHg s2/ml - Pa . s2/mm3
 
         !Elt 7 5-11 PA1-PA2, standard 2 way flow
-        !elem_field_fetal(ne_group,7) = 3.0_dp !R_Q-L-unit
+        elem_field_fetal(ne_group,7) = 3.0_dp !R_Q-L-unit
         elem_field_fetal(ne_resist,7) = 0.00933256576_dp ! Pa s /mm3 RPA
         elem_field_fetal(nef_K,7) = 0.0_dp
         elem_field_fetal(nef_L,7) = 0.002_dp*133.0_dp/1000.0_dp
 
         !Elt 8 6-7 AA-AO1, standard 2 way flow, not simple R-Qf
-        !elem_field_fetal(ne_group,8) = 3.0_dp !R-Q-L unit
+        elem_field_fetal(ne_group,8) = 3.0_dp !R-Q-L unit
         elem_field_fetal(ne_resist,8) = 0.01599868416_dp ! Pa s /mm3 !AA Resistance
         elem_field_fetal(nef_K,8) = 0.0_dp
         elem_field_fetal(nef_L,8) = 0.002_dp*133.0_dp/1000.0_dp !mmHg s2/ml - Pa . s2/mm3
 
         !Elt 9 7-8 AO1-AO2 standard 2 way flow
-        !elem_field_fetal(ne_group,9) = 2.0_dp !Simple R-Q unit
+        elem_field_fetal(ne_group,9) = 2.0_dp !Simple R-Q unit
         elem_field_fetal(ne_resist,9) =0.0533289472_dp ! Pa s /mm3 RIsthm
         elem_field_fetal(nef_K,9) = 0.0_dp
         elem_field_fetal(nef_L,9) = 0.0_dp
 
         !Elt 10 7-13 AO1-CA, CARO
-        !elem_field_fetal(ne_group,10) = 3.0_dp !R-Q-L unit
+        elem_field_fetal(ne_group,10) = 3.0_dp !R-Q-L unit
         elem_field_fetal(ne_resist,10) = 0.0399967104_dp ! Pa s /mm3
         elem_field_fetal(nef_K,10) = 0.0_dp
         elem_field_fetal(nef_L,10) = 0.08_dp*133.0_dp/1000.0_dp
 
         !Elt 11 7-16 AO1-UB - RUBA
-        !elem_field_fetal(ne_group,11) = 2.0_dp !Simple R-Q unit
+        elem_field_fetal(ne_group,11) = 2.0_dp !Simple R-Q unit
         elem_field_fetal(ne_resist,11) = 1.066578944_dp ! Pa s /mm3
         elem_field_fetal(nef_K,11) = 0.0_dp
         elem_field_fetal(nef_L,11) = 0.0_dp
 
         !Elt 12 8-9 AO2-AO3,
-        !elem_field_fetal(ne_group,12) = 2.0_dp !Simple R-Q unit
+        elem_field_fetal(ne_group,12) = 2.0_dp !Simple R-Q unit
         elem_field_fetal(ne_resist,12) = 0.00533289472_dp ! Pa s /mm3 R_DTAO
         elem_field_fetal(nef_K,12) = 0.0_dp
         elem_field_fetal(nef_L,12) = 0.0_dp
@@ -283,7 +363,7 @@ contains
         elem_field_fetal(nef_K,32) = 0.0_dp
         elem_field_fetal(nef_L,32) = 0.0_dp
 
-        print *, elem_field_fetal(nef_K,13)
+        print *, elem_field_fetal(ne_group,:)
         !write(*,*) 'Calculating placental resistance'
         !mesh_type = 'simple_tree'
         !elem_field(ne_viscfact,:) = 1.0_dp !initialise viscosity factor
@@ -305,35 +385,37 @@ contains
            np_in = elem_nodes_fetal(1,ne)
            np_out = elem_nodes_fetal(2,ne)
            Pgrad = node_field_fetal(njf_press,np_in)-node_field_fetal(njf_press,np_out)
-            if (elem_field_fetal(ne_group,ne).eq.2.0_dp)then!R-Q unit
+            if (abs(elem_field_fetal(ne_group,ne)-2.0_dp).lt.loose_tol)then!R-Q unit
+            !if (elem_field_fetal(ne_group,ne).eq.2.0_dp)then
+                write(*,*) ne,elem_field_fetal(ne_group,ne)-2.0_dp,'group2'
                call rq_unit(dt,elem_field_fetal(ne_Qdot,ne),Pgrad,elem_field_fetal(ne_resist,ne))
-            else if (elem_field_fetal(ne_group,ne).eq.3.0_dp)then!R-Q-L units, initialise to zero L and will be time stepped units
+            else if (abs(elem_field_fetal(ne_group,ne)-3.0_dp).lt.loose_tol)then!R-Q-L units, initialise to zero L and will be time stepped units
                 call rq_unit(dt,elem_field_fetal(ne_Qdot,ne),Pgrad,elem_field_fetal(ne_resist,ne))
                 !write(*,*) ne,np_in, np_out,elem_field_fetal(ne_Qdot,ne),Pgrad
-            elseif (elem_field_fetal(ne_group,ne).eq.4.0_dp)then!FOROMAN OVALE
+            elseif (abs(elem_field_fetal(ne_group,ne)-4.0_dp).lt.loose_tol)then!FOROMAN OVALE
                if(Pgrad.le.0.0_dp)then
                    elem_field_fetal(ne_Qdot,ne)=0.0_dp
                else
                    elem_field_fetal(ne_Qdot,ne)=(Pgrad/elem_field_fetal(nef_k,ne))**(1.0_dp/0.625_dp)
                end if
                !write(*,*) ne,np_in, np_out,elem_field_fetal(ne_Qdot,ne),Pgrad
-             elseif  (elem_field_fetal(ne_group,ne).eq.5.0_dp)then!R-Q-K unit (ductus venosus
+             elseif  (abs(elem_field_fetal(ne_group,ne)-5.0_dp).lt.loose_tol)then!R-Q-K unit (ductus venosus
                 call rqk_unit(dt,elem_field_fetal(ne_Qdot,ne),Pgrad,elem_field_fetal(ne_resist,ne),&
                            elem_field_fetal(nef_K,ne))
-             elseif  (elem_field_fetal(ne_group,ne).eq.6.0_dp)then!R-Q-K-L unit (ductus arterious, will time step
+             elseif  (abs(elem_field_fetal(ne_group,ne)-6.0_dp).lt.loose_tol)then!R-Q-K-L unit (ductus arterious, will time step
                 call rqk_unit(dt,elem_field_fetal(ne_Qdot,ne),Pgrad,elem_field_fetal(ne_resist,ne),&
                            elem_field_fetal(nef_K,ne))
                 !write(*,*) ne,np_in, np_out,elem_field_fetal(ne_Qdot,ne),Pgrad
-             elseif  (elem_field_fetal(ne_group,ne).eq.8.0_dp)then!Cardiac exits
+             elseif  (abs(elem_field_fetal(ne_group,ne)-8.0_dp).lt.loose_tol)then!Cardiac exit
                 if(Pgrad.lt.0.0_dp)then
                     elem_field_fetal(ne_Qdot,ne)=0.0_dp
                 else
                     call rqk_unit(dt,elem_field_fetal(ne_Qdot,ne),Pgrad,elem_field_fetal(ne_resist,ne),&
                            elem_field_fetal(nef_K,ne))
                 end if
-                !write(*,*) ne,np_in, np_out,elem_field_fetal(ne_Qdot,ne),Pgrad
+            else
             end if
-            !write(*,*) ne,np_in, np_out,elem_field_fetal(ne_Qdot,ne),Pgrad
+            write(*,*) ne,np_in, np_out,elem_field_fetal(ne_Qdot,ne),elem_field_fetal(ne_group,ne),Pgrad
         end do
 
         time = 0.0_dp !initialise the simulation time.
@@ -407,14 +489,20 @@ contains
                     node_field_fetal(njf_netQ,np)=Qnod
                     Vnod = node_field_fetal(njf_vol,np)+dt*Qnod
                     node_field_fetal(njf_vol,np) = Vnod
+                    if((n.eq.1).and.(ttime.eq.dt))then
+                        write(*,*) np,node_field_fetal(njf_type,np),press,Qnod,vnod
+                    end if
                     !write(*,*) np,Qnod,node_field_fetal(njf_vol,np),node_field_fetal(njf_press,np)
-                if(node_field_fetal(njf_type,np).eq.1.0_dp)then!right ventricle
+                    if(abs(node_field_fetal(njf_type,np)-1.0_dp).lt.loose_tol)then!right ventricle
                         call ventricle_pressure_step(press,Avent,U0RV,EdiaRV,EsysRV,RvRV,Qnod,Vnod)
                         node_field_fetal(njf_press,np) = press
-                    elseif(node_field_fetal(njf_type,np).eq.2.0_dp)then!left ventricle)
+                        if((n.eq.1).and.(ttime.eq.dt))then
+                            write(*,*) np,press
+                        end if
+                    elseif(abs(node_field_fetal(njf_type,np)-2.0_dp).lt.loose_tol)then!left ventricle)
                         call ventricle_pressure_step(press,Avent,U0LV,EdiaLV,EsysLV,RvLV,Qnod,Vnod)
                         node_field_fetal(njf_press,np) = press
-                    elseif(node_field_fetal(njf_type,np).eq.3.0_dp)then!Its an atrium
+                    elseif(abs(node_field_fetal(njf_type,np)-3.0_dp).lt.loose_tol)then!Its an atrium
                         call atrium_pressure_step(press,dt,Aatria,U0A,node_field_fetal(njf_comp,np),Qnod,dQ,Vnod)
                         node_field_fetal(njf_press,np) = press
                     else ! This is a standard node
@@ -430,32 +518,34 @@ contains
                     np_in = elem_nodes_fetal(1,ne)
                     np_out = elem_nodes_fetal(2,ne)
                     Pgrad = node_field_fetal(njf_press,np_in)-node_field_fetal(njf_press,np_out)
-                    if (elem_field_fetal(ne_group,ne).eq.2.0_dp)then!R-Q unit
+                    if (abs(elem_field_fetal(ne_group,ne)-2.0_dp).lt.loose_tol)then!R-Q unit
                        call rq_unit(dt,elem_field_fetal(ne_Qdot,ne),Pgrad,elem_field_fetal(ne_resist,ne))
-                    else if (elem_field_fetal(ne_group,ne).eq.3.0_dp)then!R-Q-L units, initialise to zero L and will be time stepped units
+                    else if (abs(elem_field_fetal(ne_group,ne)-3.0_dp).lt.loose_tol)then!R-Q-L units, initialise to zero L and will be time stepped units
                         call rql_unit(dt,elem_field_fetal(ne_Qdot,ne),Pgrad,elem_field_fetal(ne_resist,ne),&
                             elem_field_fetal(nef_L,ne))
-                    elseif (elem_field_fetal(ne_group,ne).eq.4.0_dp)then!FOROMAN OVALE
+                    elseif (abs(elem_field_fetal(ne_group,ne)-4.0_dp).lt.loose_tol)then!FOROMAN OVALE
                         if(Pgrad.le.0.0_dp)then
                            elem_field_fetal(ne_Qdot,ne)=0.0_dp
                         else
                            elem_field_fetal(ne_Qdot,ne)=(Pgrad/elem_field_fetal(nef_k,ne))**(1.0_dp/0.625_dp)
                         end if
-                    elseif  (elem_field_fetal(ne_group,ne).eq.5.0_dp)then!R-Q-K unit (ductus venosus
+                    elseif  (abs(elem_field_fetal(ne_group,ne)-5.0_dp).lt.loose_tol)then!R-Q-K unit (ductus venosus
                         call rqk_unit(dt,elem_field_fetal(ne_Qdot,ne),Pgrad,elem_field_fetal(ne_resist,ne),&
                            elem_field_fetal(nef_K,ne))
-                     elseif  (elem_field_fetal(ne_group,ne).eq.6.0_dp)then!R-Q-K-L unit (ductus arterious, will time step
+                     elseif  (abs(elem_field_fetal(ne_group,ne)-6.0_dp).lt.loose_tol)then!R-Q-K-L unit (ductus arterious, will time step
                         call rqkl_unit(dt,elem_field_fetal(ne_Qdot,ne),Pgrad,elem_field_fetal(ne_resist,ne),&
                                elem_field_fetal(nef_K,ne),elem_field_fetal(nef_L,ne))
-                     elseif  (elem_field_fetal(ne_group,ne).eq.8.0_dp)then!Cardiac exits
+                     elseif  (abs(elem_field_fetal(ne_group,ne)-8.0_dp).lt.loose_tol)then!Cardiac exits
+                        !write(*,*) ne, elem_field_fetal(ne_group,ne), elem_field_fetal(ne_Qdot,ne)
                         if(Pgrad.lt.0.0_dp)then
                             elem_field_fetal(ne_Qdot,ne)=0.0_dp
                         else
-                            !write(*,*) 'flow through exit',ne
                             call rqk_unit(dt,elem_field_fetal(ne_Qdot,ne),Pgrad,elem_field_fetal(ne_resist,ne),&
                                elem_field_fetal(nef_K,ne))
                         end if
-                    elseif  (elem_field_fetal(ne_group,ne).eq.1.0_dp)then!Cardiac valves
+                        !write(*,*) ne, elem_field_fetal(ne_group,ne), elem_field_fetal(ne_Qdot,ne)
+                        !write(*,*) ne,np_in, np_out,elem_field_fetal(ne_Qdot,ne),Pgrad
+                    elseif  (abs(elem_field_fetal(ne_group,ne)-1.0_dp).lt.loose_tol)then!Cardiac valves
                         if(Pgrad.lt.0.0_dp)then
                             elem_field_fetal(ne_Qdot,ne)=0.0_dp
                         else
